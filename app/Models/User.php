e61a806a9c34d627b35d52e640612cdca6077ed4
ArturoMoralesDev <ramon.morales41@gmail.com>
2025-02-19 09:48:46 -0600

    Ajustar modelo de user.

--- app/Models/User.php
+++ app/Models/User.php
@@ -129,6 +129,9 @@ class User extends Authenticatable
         return $this->hasMany(Sale::class);
     }
 
-
+    public function customer()
+    {
+        return $this->hasOne(Customer::class);
+    }
 
 }
